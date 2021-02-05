ccb7d35709035cfacb9ca586f546efbcd0ab0ed8
nikunj goyal <nikunjgoyal31@gmail.com>
2021-02-06 01:05:32 +0530

    first commmit

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+const App = (_) => {
+  const url = "https://jsonplaceholder.typicode.com/comments";
+
+  const [comments, setComments] = useState([]);
+
+  const getAllComments = () => {
+    axios
+      .get(url, {
+        params: {
+          _limit: 13,
+        },
+      })
+      .then((response) => {
+        const allComments = response.data;
+
+        setComments(allComments);
+        console.log(comments);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const listComments = comments.map((comment) => (
+    <span>
+      {comment.id}: {comment.email}
+      <br />
+    </span>
+  ));
+  // useEffect(() => {
+  //   getAllComments();
+  // }, []);
+
+  const handleClick = () => {
+    getAllComments();
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <div className="box">{listComments}</div>
+        <div className="button" onClick={handleClick}>
+          Pull Data
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
